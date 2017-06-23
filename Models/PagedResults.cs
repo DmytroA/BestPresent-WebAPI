8446dbca35be6b32b45693432eb67207cee374f0
dmytro andriichuk <dmytro.andriichuk@4tree.de>
2017-06-23 16:24:05 +0300

    BP-1 add pagination

---
+++ Models/PagedResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BestPresent.WebAPI.Models
+{
+    public class PagedResults<T>
+    {
+        public Pagination Pagination { get; set; }
+        public IEnumerable<T> data { get; set; }
+    }
+}
