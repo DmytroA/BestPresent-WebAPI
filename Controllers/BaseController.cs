74948ce1400e46c0c57236d673b48b31ec5428e2
dmytro andriichuk <dmytro.andriichuk@4tree.de>
2017-06-26 16:11:46 +0300

    BP-4 move pagination to base controller

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper.QueryableExtensions;
+using BestPresent.WebAPI.Extensions;
+using BestPresent.WebAPI.Models;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace BestPresent.WebAPI.Controllers
+{
+    public abstract class BaseController : ApiController
+    {
+        protected async Task<PagedResults<TReturn>> CreatePagedResults<T, TReturn>(
+           IQueryable<T> queryable,
+           int page,
+           int pageSize,
+           string orderBy,
+           bool ascending)
+        {
+            var skipAmount = pageSize * (page - 1);
+
+            var projection = queryable
+                .OrderByPropertyOrField(orderBy, ascending)
+                .Skip(skipAmount)
+                .Take(pageSize).ProjectTo<TReturn>();
+
+            var totalNumberOfRecords = queryable.Count();
+            var results = projection.ToList();
+
+            var mod = totalNumberOfRecords % pageSize;
+            var totalPageCount = (totalNumberOfRecords / pageSize) + (mod == 0 ? 0 : 1);
+
+            return new PagedResults<TReturn>
+            {
+                data = results,
+                Pagination = new Pagination
+                {
+                    PageNumber = page,
+                    PageSize = results.Count,
+                    TotalNumberOfPages = totalPageCount,
+                    TotalNumberOfRecords = totalNumberOfRecords,
+                },
+            };
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,20 +3,13 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Cors;
-using System.Collections.Generic;
-using Newtonsoft.Json;
-using System;
-using System.IO;
-using System.Net.Http.Headers;
 using BestPresent.WebAPI.Models;
 using System.Threading.Tasks;
-using BestPresent.WebAPI.Extensions;
-using AutoMapper.QueryableExtensions;
 
 namespace BestPresent.WebAPI.Controllers
 {
     [EnableCors("*", "*", "*")]
-    public class HomeController : ApiController
+    public class HomeController : BaseController
     {
         Entities context = new Entities();
 
@@ -39,37 +32,7 @@ namespace BestPresent.WebAPI.Controllers
             (context.Hotels, page.Value, pageSize, orderBy, ascending);
             return Ok(hotel);
         }
-        protected async Task<PagedResults<TReturn>> CreatePagedResults<T, TReturn>(
-            IQueryable<T> queryable,
-            int page,
-            int pageSize,
-            string orderBy,
-            bool ascending)
-        {
-            var skipAmount = pageSize * (page - 1);
-
-            var projection = queryable
-                .OrderByPropertyOrField(orderBy, ascending)
-                .Skip(skipAmount)
-                .Take(pageSize).ProjectTo<TReturn>();
-
-            var totalNumberOfRecords = queryable.Count();
-            var results = projection.ToList();
-
-            var mod = totalNumberOfRecords % pageSize;
-            var totalPageCount = (totalNumberOfRecords / pageSize) + (mod == 0 ? 0 : 1);
-
-            return new PagedResults<TReturn>
-            {
-                data = results,
-                Pagination = new Pagination {
-                    PageNumber = page,
-                    PageSize = results.Count,
-                    TotalNumberOfPages = totalPageCount,
-                    TotalNumberOfRecords = totalNumberOfRecords,
-                },
-            };
-        }
+       
         public HttpResponseMessage Get(int id)
         {
             var hotel = context.Hotels.First(p => p.Id == id);
